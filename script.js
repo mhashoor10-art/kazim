7dec3910084376f9d48647ff11b5feb9e1cca7ff
mhashoor10-art <mhashoor10@gmail.com>
2026-03-28 23:39:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ loadBlogs();
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 50,
+        behavior: "smooth"
+      });
+    }
+  });
+});
