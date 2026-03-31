b8f1a196223f46eb92c5321c65650f6d5f514c00
mhashoor10-art <mhashoor10@gmail.com>
2026-03-31 22:21:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Config (same as admin)
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBlQP-s0Q-y6J1POkEEHrcDP32Wn6JPK_4",
   authDomain: "kazim-aa621.firebaseapp.com",
@@ -18,26 +18,30 @@ async function loadBlogs() {
 
   if (!container) return;
 
-  container.innerHTML = "Loading...";
+  container.innerHTML = "Loading blogs...";
 
-  const data = await getDocs(collection(db, "blogs"));
+  try {
+    const data = await getDocs(collection(db, "blogs"));
 
-  container.innerHTML = "";
+    container.innerHTML = "";
 
-  data.forEach((doc) => {
-    const blog = doc.data();
+    data.forEach((doc) => {
+      const blog = doc.data();
 
-    container.innerHTML += `
-      <div class="card">
-        <a href="post.html?id=${doc.id}">
-          <img src="${blog.img}" style="width:100%; border-radius:10px;">
-          <h3>${blog.title}</h3>
-          <p>${blog.desc}</p>
-          <small>${blog.date}</small>
-        </a>
-      </div>
-    `;
-  });
+      container.innerHTML += `
+        <div class="card">
+          <img src="${blog.img || "https://via.placeholder.com/400"}" style="width:100%; border-radius:10px;">
+          <h3>${blog.title || "No Title"}</h3>
+          <p>${blog.desc || ""}</p>
+          <small>${blog.date || ""}</small>
+        </div>
+      `;
+    });
+
+  } catch (error) {
+    console.error("Error loading blogs:", error);
+    container.innerHTML = "Failed to load blogs.";
+  }
 }
 
 // Run
